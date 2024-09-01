4ddfe1aad27f57d3c3fab13a46b0109542782a2a
Kerry Patrick <softwareliberationarmy@gmail.com>
2024-09-01 13:45:11 -0500

    missed one of the Gameboard references

--- src/Minesweeper.test.tsx
+++ src/Minesweeper.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import React from "react";
-import Gameboard from "./Minesweeper";
+import Minesweeper from "./Minesweeper";
 import userEvent from "@testing-library/user-event";
 
 const mockInitialBombMap = jest.fn();
@@ -17,30 +17,30 @@ const bomb = cell("💣");
 const oneByOneBombMap = () => [[bomb]];
 const oneByOneSuccessMap = () => [[cell("0")]];
 
-describe("Gameboard", () => {
+describe("Minesweeper game", () => {
   mockInitialBombMap.mockReturnValue([
     [bomb, cell("1")],
     [cell("1"), cell("1")],
   ]);
   describe("on initial render", () => {
     it("should show the game name", () => {
-      render(<Gameboard />);
+      render(<Minesweeper />);
       expect(screen.getByText("Minesweeper")).toBeInTheDocument();
     });
 
     it("should show the game board", () => {
-      render(<Gameboard />);
+      render(<Minesweeper />);
       expect(screen.getByTestId("game-board")).toBeInTheDocument();
     });
 
     it("should show all the buttons on the game board", () => {
-      render(<Gameboard rows={2} columns={2} />);
+      render(<Minesweeper rows={2} columns={2} />);
       const buttons = screen.getAllByRole("button");
       expect(buttons).toHaveLength(4);
     });
 
     it("should not show a play again button", () => {
-      render(<Gameboard rows={2} columns={2} />);
+      render(<Minesweeper rows={2} columns={2} />);
       expect(screen.queryByText("Play again")).not.toBeInTheDocument();
     });
   });
@@ -49,7 +49,7 @@ describe("Gameboard", () => {
     it("should show the number under the button when you click the button", async () => {
       mockInitialBombMap.mockReturnValue(oneByOneSuccessMap());
       const user = userEvent.setup();
-      render(<Gameboard rows={1} columns={1} />);
+      render(<Minesweeper rows={1} columns={1} />);
       expect(screen.queryByText("0")).not.toBeInTheDocument();
       expect(screen.getByRole("button")).toBeInTheDocument();
       await user.click(screen.getByRole("button"));
@@ -63,7 +63,7 @@ describe("Gameboard", () => {
     it("shows a failure message when you click on a bomb", async () => {
       mockInitialBombMap.mockReturnValue(oneByOneBombMap());
       const user = userEvent.setup();
-      render(<Gameboard rows={1} columns={1} />);
+      render(<Minesweeper rows={1} columns={1} />);
       expect(screen.getByText("Good luck!")).toBeInTheDocument();
 
       await user.click(screen.getByRole("button"));
@@ -76,7 +76,7 @@ describe("Gameboard", () => {
     it("shows a success message when you click on all non-bomb cells", async () => {
       mockInitialBombMap.mockReturnValue(oneByOneSuccessMap());
       const user = userEvent.setup();
-      render(<Gameboard rows={1} columns={1} />);
+      render(<Minesweeper rows={1} columns={1} />);
       expect(screen.getByText("Good luck!")).toBeInTheDocument();
 
       await user.click(screen.getByRole("button"));
@@ -91,7 +91,7 @@ describe("Gameboard", () => {
         [bomb, cell("1")],
         [cell("1"), cell("1")],
       ]);
-      render(<Gameboard rows={2} columns={2} />);
+      render(<Minesweeper rows={2} columns={2} />);
       const button = screen.getAllByRole("button")[0];
       userEvent.click(button);
       await waitFor(() => {
@@ -102,7 +102,7 @@ describe("Gameboard", () => {
     it("shows a play again button when you lose", async () => {
       mockInitialBombMap.mockReturnValue(oneByOneBombMap());
       const user = userEvent.setup();
-      render(<Gameboard rows={1} columns={1} />);
+      render(<Minesweeper rows={1} columns={1} />);
       expect(screen.getByText("Good luck!")).toBeInTheDocument();
 
       await user.click(screen.getByRole("button"));
@@ -117,7 +117,7 @@ describe("Gameboard", () => {
     it("resets the gameboard when you click play again", async () => {
       mockInitialBombMap.mockReturnValue(oneByOneBombMap());
       const user = userEvent.setup();
-      render(<Gameboard rows={1} columns={1} />);
+      render(<Minesweeper rows={1} columns={1} />);
       expect(screen.getByText("Good luck!")).toBeInTheDocument();
 
       await user.click(screen.getByRole("button"));
